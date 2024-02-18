dad8c192aa329c977928bd61bb3aadeca44ae547
Voslo <rewqvolty228@gmail.com>
2024-02-18 21:53:16 +0200

    all tasks complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,26 @@ import { useState, useEffect } from 'react';
 import { ContactList } from './components/ContactList/ContactList';
 import {ContactForm} from './components/Form/ContactForm';
 import { SearchBox } from './components/SearchBox/SearchBox';
-  
-function App() {
-      const defaultContacts = [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ];
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from './redux/contacsSlice';
+import { deleteContact } from './redux/contacsSlice';  
 
-  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')) || defaultContacts)
+function App() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts)
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  const addContact = (newContact) => {
-    setContacts([...contacts, newContact]);
+  const addContactOld = (newContact) => {
+    dispatch(addContact(newContact))
   };
 
-  const deleteContact = (contactId) => {
-    setContacts(contacts.filter(contact => contact.id !== contactId));
-  };
+  const deleteContactOld = (contactId) => {
+    dispatch(deleteContact(contactId))
+  }
 
   const changeFilter = (e) => {
     setFilter(e.target.value);
@@ -40,9 +37,9 @@ function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm onAddContact={addContact} />
+      <ContactForm onAddContact={addContactOld} />
       <SearchBox value={filter} onChange={changeFilter} />
-      <ContactList contacts={getVisibleContacts()} onDeleteContact={deleteContact} />
+      <ContactList contacts={getVisibleContacts()} onDeleteContact={deleteContactOld} />
     </div>
   );
 }
